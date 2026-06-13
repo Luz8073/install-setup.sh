f038997c82e19c980a424de141a3cdb5947083d6
Luz <81642250+Luz8073@users.noreply.github.com>
2026-06-13 12:39:03 +0100

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo pacman -S curl discord docker steam zed obsidian obs-studio flatpak pavucontrol gpu-screen-recorder
+sudo yay -S zen-browser spicetify-cli r2modman android-studio
+
+
+curl https://sh.rustup.rs -sSf | sh
+
+
+flatpak install flathub org.vinegarhq.Sober
